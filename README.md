46fb5ae3c40aac36c6d57c2998491b0b23f298b3
George Iwu <c.georgeiwu@gmail.com>
2022-07-16 21:10:36 -0400

    first commit
    first commit
    
    first commit

--- README.md
+++ README.md
@@ -1,30 +1,35 @@
-# service_catalog
+# Flight Tracker
+  API to process array of flights and generate routes for a given person
 
 ## Description
-This is an example of implementation of Service catalog
+  API Method
+    ``POST /track``
 
 
-## Description
+  API Request Params (JSON Format)
+     * source `string`
+     * flights `array of strings (source and destination)`
 
-Following the Clean Architecture by Uncle Bob
- * Independent of Frameworks. The architecture does not depend on the existence of some library of feature laden software. This allows you to use such frameworks as tools, rather than having to cram your system into their limited constraints.
- * Testable. The business rules can be tested without the UI, Database, Web Server, or any other external element.
- * Independent of UI. The UI can change easily, without changing the rest of the system. A Web UI could be replaced with a console UI, for example, without changing the business rules.
- * Independent of Database. You can swap out Oracle or SQL Server, for Mongo, BigTable, CouchDB, or something else. Your business rules are not bound to the database.
- * Independent of any external agency. In fact your business rules simply don’t know anything at all about the outside world.
+     *Example
+        ```
+          {
+              "source": "SFO",
+              "flights": [
+                  ["IND", "EWR"], ["SFO", "ATL"], ["GSO", "IND"], ["ATL", "GSO"]
+              ]
+          }
+        ```
 
-More at https://8thlight.com/blog/uncle-bob/2012/08/13/the-clean-architecture.html
+  API Response (JSON Format)
+     * airport quote `array of strings for stops`
+        ```
+          [
+            "SFO",
+            "ATL",
+            "GSO",
+            "IND",
+            "EWR"
+          ]
+        ```
 
-This project has  4 Domain layer :
- * Domain Layer
- * Repository(DB) Layer
- * Usecase(Service) Layer
- * Handler(Controller) Layer
 
-
-Other assumptions
-  * A default version is created whenever a service is created
-  * Creating another version of a service calls the create version endpoint
-  * The direction of sort should be in descending order
-  * Items on each page limited to 12
-  * Used entgo orm for easy database access
--- main.go
+++ main.go
@@ -2,50 +2,120 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"sort"
+	"strings"
 	"time"
 
-	_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo/v4"
-
-	"service-catalog/ent"
-	"service-catalog/middleware"
-	"service-catalog/service"
 )
 
-//move to environment variable
-const (
-	dbHost     = "localhost"
-	dbUser     = "kong"
-	dbPassword = "password"
-	dbType    = "mysql"
-	dbProtocol = "tcp"
-	dbName = "catalog"
-)
+type RequestData struct {
+	Flights [][]string `json:"flights"`
+	Source  string      `json:"source"`
+}
 
 func main() {
-	sqlInfo := fmt.Sprintf("%s:%s@%s(%s)/%s?parseTime=True", dbUser, dbPassword, dbProtocol, dbHost, dbName)
-	client, err := ent.Open(dbType, sqlInfo)
-	if err != nil {
-			log.Fatalf("failed opening connection to sqlite: %v", err)
-	}
-	defer client.Close()
-	// Run the auto migration tool.
-	if err := client.Schema.Create(context.Background()); err != nil {
-			log.Fatalf("failed creating schema resources: %v", err)
-	}
 
 	e := echo.New()
-	middL := middleware.InitMiddleware()
-	e.Use(middL.Auth)
-	timeoutContext := time.Duration(60) * time.Second
-	repo := service.NewServiceRepo(client, timeoutContext)
-	usecase := service.NewServiceUsecase(repo)
-	service.NewServiceHandler(e, usecase)
-
+	e.POST("/track", Track)
 
 	fmt.Printf("Running app on port 8000")
 	log.Fatal(http.ListenAndServe(":8000", e))
+}
+
+// Controller to Track user
+func Track(c echo.Context) (err error) {
+	ctx := c.Request().Context()
+	timeoutContext := time.Duration(60) * time.Second
+	ctx, cancel := context.WithTimeout(ctx, timeoutContext)
+	defer cancel()
+
+	jsonBody := make(map[string]interface{})
+	if err := json.NewDecoder(c.Request().Body).Decode(&jsonBody); err != nil {
+		return c.JSON(http.StatusUnprocessableEntity, err.Error())
+	}
+
+	allFlights, ok := jsonBody["flights"].([]interface{})
+	if !ok {
+		return c.JSON(http.StatusUnprocessableEntity, errors.New("error"))
+	}
+	flights := make([][]string, len(allFlights))
+	for i, v := range allFlights {
+		flights[i] = make([]string, 2)
+		for y, x := range v.([]interface{}) {
+			flights[i][y] = x.(string)
+		}
+	}
+
+	source, ok := jsonBody["source"].(string)
+	if !ok {
+		return c.JSON(http.StatusUnprocessableEntity, errors.New("error"))
+	}
+	request := &RequestData{
+		Flights: flights,
+		Source:  source,
+	}
+
+	flightPaths := findRoutes(*request)
+	fmt.Println(flightPaths)
+
+	return c.JSON(http.StatusOK, flightPaths)
+}
+
+// Find Route function
+func findRoutes(data RequestData) []string {
+	m := createRouteMap(data.Flights)
+
+	route := []string{}
+	DFSearch(m, &route, data.Source)
+
+	return reverseStrings(route)
+}
+
+// Depth Frist Search
+func DFSearch(m map[string][]string, route *[]string, curr string) {
+	for len(m[curr]) > 0 {
+		next := m[curr][0]
+		m[curr] = m[curr][1:]
+
+		DFSearch(m, route, next)
+	}
+
+	*route = append(*route, curr)
+}
+
+// Create Route Map
+func createRouteMap(tickets [][]string) map[string][]string {
+	m := make(map[string][]string)
+
+	for _, ticket := range tickets {
+		to, from := ticket[0], ticket[1]
+		m[to] = append(m[to], from)
+	}
+
+	for key, conns := range m {
+		sort.Slice(m[key], func(i, j int) bool {
+			return strings.Compare(conns[i], conns[j]) == -1
+		})
+	}
+
+	return m
+}
+
+// Reverse Routes
+func reverseStrings(s []string) []string {
+	i, j := 0, len(s)-1
+	for i < j {
+		s[i], s[j] = s[j], s[i]
+
+		i++
+		j--
+	}
+
+	return s
 }
